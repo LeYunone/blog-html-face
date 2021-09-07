47b996a78039135fa569db84dba2e087daec5b0f
leyunone <365627310@qq.com>
2021-09-07 16:03:11 +0800

    [update] tag

--- src/main.js
+++ src/main.js
@@ -15,9 +15,13 @@ import Prism from 'prismjs';
 
 import VMdPreviewHtml from '@kangc/v-md-editor/lib/preview-html';
 import '@kangc/v-md-editor/lib/style/preview-html.css';
+import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index';
+import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css';
+
 VueMarkdownEditor.use(vuepressTheme, {
     Prism,
 });
+VueMarkdownEditor.use(createTodoListPlugin());
 
 const app = createApp(App)
 installElementPlus(app)

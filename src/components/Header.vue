c1e67b19ddbd2a02ae25a6c71719a35c871b42a8
leyunone <365627310@qq.com>
2021-09-09 17:28:02 +0800

    [update] tag

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,15 +6,68 @@
             <i v-else class="el-icon-s-unfold"></i>
         </div>
         <div class="header-right">
-            <a class="el-icon-search"></a>
+            <a @click="search=true" class="el-icon-search"></a>
         </div>
     </div>
+    <el-drawer
+            v-model="search"
+            direction="ttb"
+            >
+        <div class="search-from">
+            <el-input :input=input(title)  v-model="title" autocomplete="off" autocorrect="off" spellcheck="false" type="search" placeholder="搜索..." />
+        </div>
+        <div class="search-content">
+
+        </div>
+
+        <div class="block">
+            <el-pagination
+                    :hide-on-single-page="value"
+                    layout="prev, pager, next"
+                    :current-page="query.pageIndex"
+                    :page-size="query.pageSize"
+                    :total="pageTotal"
+                    >
+            </el-pagination>
+        </div>
+    </el-drawer>
 </template>
 <script>
 import { computed, onMounted } from "vue";
 import { useStore } from "vuex";
-import { useRouter } from "vue-router";
+import axios from "axios";
+import { ref, reactive } from "vue";
 export default {
+    data() {
+        return {
+            search: false,
+            direction: 'rtl',
+            title:"",
+            query:{
+                pageIndex: 1,
+                pageSize: 10,
+            },
+            pageTotal:"0",
+            value:false,
+        };
+    },
+    methods: {
+        handleClose(done) {
+        },
+        input(title){
+            if(title.length==0){
+                this.ifPage=false
+            }
+            axios({
+                url:"/leyuna/blog/search",
+                params:{
+                    kry:title,
+                    index:this.query.pageIndex,
+                    size:this.query.pageSize
+                },
+            })
+        },
+    } ,
     setup() {
         const store = useStore();
         const collapse = computed(() => store.state.collapse);
@@ -36,6 +89,27 @@ export default {
 };
 </script>
 <style scoped>
+    ::v-deep .el-input__inner{
+        height: 65px;
+        background: 0 0;
+        color: #333;
+        font-size: 1.5rem;
+        margin-left: 500px;
+        border-radius: 3rem;
+        padding: 1rem 1.5rem;
+        border: 1px solid #999;
+        box-sizing: border-box;
+        width: 90%;
+        max-width: 800px;
+        font-family: 'Songti SC','Noto Serif SC',STZhongsong,STKaiti,KaiTi,Roboto,serif;
+        font-weight: 900;
+        text-align: center;
+    }
+    .block{
+        text-align: center;
+        margin-right: 150px;
+        margin-top: 20px;
+    }
 .header {
     position: relative;
     box-sizing: border-box;

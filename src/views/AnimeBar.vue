b9d9b3e4ff8cb185a7e1d2c14cbbdd6d584de4a3
leyunone <365627310@qq.com>
2022-04-01 19:55:04 +0800

    [UPDATE]animeBar.vue

--- src/views/AnimeBar.vue
+++ src/views/AnimeBar.vue
@@ -6,8 +6,17 @@
             </el-breadcrumb>
         </div>
         <div class="anime_container">
-            <el-card class="anime-card">
+            <el-card  v-for="item in list" shadow="always" class="anime-card">
+                <el-popover
+                        placement="top-start"
+                        title="标题"
+                        width="200"
+                        trigger="hover"
+                        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+                    <el-button slot="reference">hover 激活</el-button>
+                </el-popover>
             </el-card>
+
         </div>
     </div>
 </template>
@@ -17,13 +26,13 @@
     export default {
         data() {
             return {
+                list: [1, 2, 3, 4, 5, 6, 7, 8, 9]
             };
         },
         mounted: function () {
-            
+
         },
-        methods: {
-        },
+        methods: {},
     };
 </script>
 

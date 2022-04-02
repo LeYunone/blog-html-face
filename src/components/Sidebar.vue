0e1cdd739f01b298b96845e6231ea9d52561a61b
pengli <365627310@qq.com>
2022-04-02 10:49:52 +0800

    [UPDATE]add popover css

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,16 +6,48 @@
                 <el-link class="myName">乐云一</el-link>
                 <el-divider>一个有趣的人</el-divider>
                 <div class="myLink">
-                    <a @click="openQQ=true" class="aLink">
-                        <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#el-icon-QQ"></use>
-                        </svg>
-                    </a>
-                    <a @click="openWX=true" class="aLink">
-                        <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#el-icon-weixin"></use>
-                        </svg>
-                    </a>
+                    <el-popover
+                            placement="right"
+                            trigger="hover"
+                            :width="300">
+                        <template #reference>
+                            <a class="aLink">
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#el-icon-QQ"></use>
+                                </svg>
+                            </a>
+                        </template>
+                        <template #default>
+
+                            <el-image style="width: 300px; height: 590px" :src="qqUrl" :fit="contain" />
+<!--                            <el-image-->
+<!--                                    style="width: 338px; height: 593px;text-align: center"-->
+<!--                                    :src="qqUrl"-->
+<!--                                    fit="cover"></el-image>-->
+                        </template>
+                    </el-popover>
+
+                    <el-popover
+                            placement="right"
+                            trigger="hover"
+                            :width="300">
+                        <template #reference>
+                            <a class="aLink">
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#el-icon-weixin"></use>
+                                </svg>
+                            </a>
+                        </template>
+                        <template #default>
+                            <el-image style="width: 294px; height: 288px" :src="wxUrl" :fit="contain" />
+
+<!--                            <el-image-->
+<!--                                    style="width: 338px; height: 593px;text-align: center"-->
+<!--                                    :src="wxUrl"-->
+<!--                                    fit="cover"></el-image>-->
+                        </template>
+                    </el-popover>
+
                     <a class="aLink" target="_blank" href="http://github.com/LeYunone">
                         <svg class="icon" aria-hidden="true">
                             <use xlink:href="#el-icon-github"></use>
@@ -26,7 +58,7 @@
                             <use xlink:href="#el-icon-shejiaotubiao-06"></use>
                         </svg>
                     </a>
-                    <a class="aLink"  target="_blank"  href="https://www.zhihu.com/people/leyuna">
+                    <a class="aLink" target="_blank" href="https://www.zhihu.com/people/leyuna">
                         <svg class="icon" aria-hidden="true">
                             <use xlink:href="#el-icon-29"></use>
                         </svg>
@@ -41,7 +73,7 @@
             </div>
         </div>
         <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#fff"
-            text-color="#333" active-text-color="#DD2828" unique-opened router>
+                 text-color="#333" active-text-color="#DD2828" unique-opened router>
             <template v-for="item in items">
                 <template v-if="item.subs">
                     <el-submenu :index="item.index" :key="item.index">
@@ -53,7 +85,8 @@
                             <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
                                 <template #title>{{ subItem.title }}</template>
                                 <el-menu-item v-for="(threeItem, i) in subItem.subs" :key="i" :index="threeItem.index">
-                                    {{ threeItem.title }}</el-menu-item>
+                                    {{ threeItem.title }}
+                                </el-menu-item>
                             </el-submenu>
                             <el-menu-item v-else :index="subItem.index" :key="subItem.index">{{ subItem.title }}
                             </el-menu-item>
@@ -69,112 +102,100 @@
             </template>
         </el-menu>
     </div>
-    <el-dialog title="QQ" v-model="openQQ">
-       <div style="text-align: center">
-           <el-image
-                   style="width: 338px; height: 593px;text-align: center"
-                   :src="qqUrl"
-                   fit="cover"></el-image>
-       </div>
-    </el-dialog>
-    <el-dialog title="微信" v-model="openWX">
-        <div style="text-align: center">
-            <el-image
-                style="width: 456px; height: 487px"
-                :src="wxUrl"
-                fit="cover"></el-image>
-        </div>
-    </el-dialog>
 </template>
 <script>
-import { computed, watch } from "vue";
-import { useStore } from "vuex";
-import { useRoute } from "vue-router";
-import headUrl from '../assets/img/head.jpg';
-import qqUrl from '../assets/img/qq.png';
-import wxUrl from '../assets/img/wx.png';
-export default {
-    data(){
-        return {
-            qqUrl:qqUrl,
-            wxUrl:wxUrl,
-            circleUrl:headUrl,
-            openQQ:false,
-            openWX:false,
-        }
-    },
-    setup() {
-        const items = [
-            {
-                icon: "el-icon-lx-home",
-                index: "/home",
-                title: "主站首页",
-            },
-            {
-                icon: "el-icon-s-flag",
-                index: "2",
-                title: "归档的选择",
-                subs: [
-                    {
-                        index: "/tag",
-                        title: "标签",
-                    },
-                    {
-                        index: "/type",
-                        title: "分类",
-                    },
-                ],
-            },{
-                icon: "el-icon-ship",
-                index: "/allblogindex",
-                title: "选个锤子老子全要"
-            },
-            {
-                icon :"el-icon-sugar",
-                index: "/animeBar",
-                title: "ANIME"
-            }
-            ,{
-                icon: "el-icon-trophy",
-                index: "/webhistory",
-                title: "网站短史"
-            }
-            ,{
-                title: "...待开发"
-            }
-        ];
+    import {computed, watch} from "vue";
+    import {useStore} from "vuex";
+    import {useRoute} from "vue-router";
+    import headUrl from '../assets/img/head.jpg';
+    import qqUrl from '../assets/img/qq.png';
+    import wxUrl from '../assets/img/wx.png';
 
-        const route = useRoute();
-        const onRoutes = computed(() => {
-            return route.path;
-        });
-        const store = useStore();
-        const collapse = computed(() => store.state.collapse);
-        return {
-            items,
-            onRoutes,
-            collapse,
-        };
-    },
-};
+    export default {
+        data() {
+            return {
+                qqUrl: qqUrl,
+                wxUrl: wxUrl,
+                circleUrl: headUrl,
+                openQQ: false,
+                openWX: false,
+            }
+        },
+        setup() {
+            const items = [
+                {
+                    icon: "el-icon-lx-home",
+                    index: "/home",
+                    title: "主站首页",
+                },
+                {
+                    icon: "el-icon-s-flag",
+                    index: "2",
+                    title: "归档的选择",
+                    subs: [
+                        {
+                            index: "/tag",
+                            title: "标签",
+                        },
+                        {
+                            index: "/type",
+                            title: "分类",
+                        },
+                    ],
+                }, {
+                    icon: "el-icon-ship",
+                    index: "/allblogindex",
+                    title: "选个锤子老子全要"
+                },
+                {
+                    icon: "el-icon-sugar",
+                    index: "/animeBar",
+                    title: "ANIME"
+                }
+                , {
+                    icon: "el-icon-trophy",
+                    index: "/webhistory",
+                    title: "网站短史"
+                }
+                , {
+                    title: "...待开发"
+                }
+            ];
+
+            const route = useRoute();
+            const onRoutes = computed(() => {
+                return route.path;
+            });
+            const store = useStore();
+            const collapse = computed(() => store.state.collapse);
+            return {
+                items,
+                onRoutes,
+                collapse,
+            };
+        },
+    };
 </script>
 
 <style>
-.sidebar {
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 70px;
-    bottom: 0;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
-}
-.sidebar::-webkit-scrollbar {
-    width: 0;
-}
-.sidebar-el-menu:not(.el-menu--collapse) {
-    width: 250px;
-}
-.sidebar > ul {
-    height: 100%;
-}
+    .sidebar {
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 70px;
+        bottom: 0;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+    }
+
+    .sidebar::-webkit-scrollbar {
+        width: 0;
+    }
+
+    .sidebar-el-menu:not(.el-menu--collapse) {
+        width: 250px;
+    }
+
+    .sidebar > ul {
+        height: 100%;
+    }
 </style>
--- src/views/AnimeBar.vue
+++ src/views/AnimeBar.vue
@@ -6,16 +6,19 @@
             </el-breadcrumb>
         </div>
         <div class="anime_container">
-            <el-card  v-for="item in list" shadow="always" class="anime-card">
-                <el-popover
-                        placement="top-start"
-                        title="标题"
-                        width="200"
-                        trigger="hover"
-                        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
-                    <el-button slot="reference">hover 激活</el-button>
-                </el-popover>
-            </el-card>
+
+            <el-popover
+                    placement="left-start"
+                    trigger="hover"
+                    v-for="item in list"
+                    :width="300">
+                <template #reference>
+                    <el-card   shadow="always" class="anime-card">
+                    </el-card>
+                </template>
+                <template #default>
+                </template>
+            </el-popover>
 
         </div>
     </div>

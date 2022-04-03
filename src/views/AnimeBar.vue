923c4bd1a52ffac0afec5400e53ccc638e1c917a
leyunone <365627310@qq.com>
2022-04-03 22:51:25 +0800

    [UPDATE]UPDATE AnimeBar.vue html

--- src/views/AnimeBar.vue
+++ src/views/AnimeBar.vue
@@ -6,17 +6,23 @@
             </el-breadcrumb>
         </div>
         <div class="anime_container">
-
             <el-popover
                     placement="left-start"
                     trigger="hover"
-                    v-for="item in list"
+                    v-for="(item,index) in animeList"
                     :width="300">
                 <template #reference>
-                    <el-card   shadow="always" class="anime-card">
+                    
+                    <el-card shadow="always" class="anime-card">
+                        <span class="anime_title">{{index}}</span>
+                        <el-image
+                                style="width: 130px; height: 150px"
+                                :src="item.blogCover"
+                                fit="fill"></el-image>
                     </el-card>
                 </template>
                 <template #default>
+                    
                 </template>
             </el-popover>
 
@@ -25,17 +31,43 @@
 </template>
 
 <script>
+    import {ElMessage, ElMessageBox} from "element-plus";
 
     export default {
         data() {
             return {
-                list: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                animeList: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                animeCount: 0,
+                form: {
+                    index: 1,
+                    size: 20
+                }
             };
         },
         mounted: function () {
-
+            this.getAnime();
         },
-        methods: {},
+        methods: {
+            getAnime() {
+                axios({
+                    url: "/leyuna/blogs",
+                    method: "GET",
+                    params: {
+                        index: this.form.index,
+                        size: this.form.size,
+                        blogType: 4
+                    }
+                }).then((res) => {
+                    var data = res.data;
+                    if (data.status) {
+                        this.animeList = data.data.records;
+                        this.animeCount = data.data.total;
+                    } else {
+                        ElMessage.error(data.message);
+                    }
+                })
+            }
+        },
     };
 </script>
 
